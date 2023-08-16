28a848f877b82b82baee8a1cea9e20f99371687a
LingHu01 <michel11.zhao@gmail.com>
2023-08-16 21:31:35 +0200

    start

---
+++ main.py
@@ -0,0 +1,22 @@
+import asyncio
+import multiprocessing
+import random
+
+async def async_task(task_name):
+    i = 0
+    for x in range(10**100000000):
+        i += 1
+        print(i)
+
+def run_async_task(task_name):
+    asyncio.run(async_task(task_name))
+
+if __name__ == '__main__':
+    if random.choice([True, False]):
+        tasks = ['Task 1', 'Task 2', 'Task 3']
+
+        with multiprocessing.Pool(processes=len(tasks)) as pool:
+            pool.map(run_async_task, tasks)
+
+    else:
+        pass
